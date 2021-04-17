8fb865a5a3021e88ee7d327a3ba9b959427db86c
Koppa Péter <koppa96@windowslive.com>
2021-04-17 18:09:25 +0200

    Remove projection from paged list extensions

---
+++ Minigram.Application/Extensions/QueryableExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Minigram.Application.Abstractions.Dtos;
+
+namespace Minigram.Application.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static async Task<PagedListDto<T>> ToPagedListAsync<T>(
+            this IQueryable<T> queryable,
+            int pageIndex,
+            int pageSize,
+            CancellationToken cancellationToken)
+        {
+            var totalCount = await queryable.CountAsync(cancellationToken);
+            List<T> results;
+            if (totalCount > 0)
+            {
+                results = await queryable.Skip(pageIndex * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync(cancellationToken);
+            }
+            else
+            {
+                results = new List<T>();
+            }
+        
+            return new PagedListDto<T>
+            {
+                TotalCount = totalCount,
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                Data = results
+            };
+        }
+    }
+}
