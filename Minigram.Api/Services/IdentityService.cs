6dac2f3df79eb87a8ac344a351a5dfd842f0ce45
Koppa Péter <koppa96@windowslive.com>
2021-04-17 14:08:32 +0200

    Add IdentityService implementation

---
+++ Minigram.Api/Services/IdentityService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Minigram.Application.Abstractions.Services;
+
+namespace Minigram.Api.Services
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly HttpContext httpContext;
+
+        private Guid? currentUserId;
+        public Guid CurrentUserId
+        {
+            get
+            {
+                return currentUserId ??=
+                    Guid.Parse(httpContext.User.Claims.Single(x => x.Type == JwtRegisteredClaimNames.Sub).Value);
+            }
+        }
+
+        public IdentityService(IHttpContextAccessor httpContextAccessor)
+        {
+            httpContext = httpContextAccessor.HttpContext;
+        }
+    }
+}
