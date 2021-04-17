c25b9b0af003fcfc1b61ad5857002215723485d7
Koppa Péter <koppa96@windowslive.com>
2021-04-17 14:07:33 +0200

    Add paging support

---
+++ Minigram.Application/Abstractions/Dtos/PagedListDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Minigram.Application.Abstractions.Dtos
+{
+    public class PagedListDto<T>
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public List<T> Data { get; set; }
+    }
+}
---
+++ Minigram.Application/Extensions/OrderedQueryableExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using Minigram.Application.Abstractions.Dtos;
+
+namespace Minigram.Application.Extensions
+{
+    public static class OrderedQueryableExtensions
+    {
+        public static Task<PagedListDto<TResult>> ToPagedListAsync<TEntity, TResult>(
+            this IOrderedQueryable<TEntity> queryable,
+            int pageIndex,
+            int pageSize,
+            Expression<Func<TEntity, TResult>> selector,
+            CancellationToken cancellationToken = default)
+        {
+            return queryable.ToPagedListAsync(
+                pageIndex,
+                pageSize,
+                entities => entities.Select(selector),
+                cancellationToken);
+        }
+        
+        public static Task<PagedListDto<TResult>> ToPagedListAsync<TEntity, TResult>(
+            this IOrderedQueryable<TEntity> queryable,
+            int pageIndex,
+            int pageSize,
+            IConfigurationProvider configurationProvider,
+            CancellationToken cancellationToken = default)
+        {
+            return queryable.ToPagedListAsync(
+                pageIndex,
+                pageSize,
+                entities => entities.ProjectTo<TResult>(configurationProvider),
+                cancellationToken);
+        }
+        
+        private static async Task<PagedListDto<TResult>> ToPagedListAsync<TEntity, TResult>(
+            this IOrderedQueryable<TEntity> queryable,
+            int pageIndex,
+            int pageSize,
+            Func<IQueryable<TEntity>, IQueryable<TResult>> mappingFunc,
+            CancellationToken cancellationToken)
+        {
+            var totalCount = await queryable.CountAsync(cancellationToken);
+            List<TResult> results;
+            if (totalCount > 0)
+            {
+                results = await mappingFunc(queryable.Skip(pageIndex * pageSize)
+                    .Take(pageSize))
+                    .ToListAsync(cancellationToken);
+            }
+            else
+            {
+                results = new List<TResult>();
+            }
+        
+            return new PagedListDto<TResult>
+            {
+                TotalCount = totalCount,
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                Data = results
+            };
+        }
+    }
+}
